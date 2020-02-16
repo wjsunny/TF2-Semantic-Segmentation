e3f6e20a2fef0a2f57bbe69b7e4b4dd20802a89a
WJsunny <jakkapan.sun@hotmail.com>
2020-02-16 18:12:33 +0700

    began encode of vgg16

--- vgg16.py
+++ vgg16.py
@@ -1,3 +1,7 @@
 import tensorflow as tf
+from tensorflow.keras. import Model
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
 
-class vgg16(tf.keras.)
+class vgg16(Model):
+    def __init__(self):
+        self.conv3
