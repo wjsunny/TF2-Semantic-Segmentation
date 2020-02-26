86ad5fbaf87923ec1b40a806fd5975c5b1a72c7c
WJsunny <jakkapan.sun@hotmail.com>
2020-02-26 21:47:02 +0700

    update utils.py, test.py

--- test.py
+++ test.py
@@ -1,75 +1,25 @@
 from __future__ import absolute_import, division, print_function
 import tensorflow as tf
+from utils import *
 import cv2
 from backbone import vgg16_2
 #from backbone import vgg16
 import numpy as np
 import matplotlib.pyplot as plt
-#from tensorflow.keras.preprocessing.image import load_img
-#from tensorflow.keras.preprocessing.image import img_to_array
-#from tensorflow.keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.applications.vgg16 import decode_predictions
 from tensorflow.keras.applications.vgg16 import VGG16
 
 
-def preprocessing_img():
-    BGR = cv2.imread('./images/som2.jpg')
-    #plt.imshow(BGR)
-    #plt.show()
-    RGB = cv2.cvtColor(BGR, cv2.COLOR_BGR2RGB)
-    #plt.imshow(RGB)
-    #plt.show()
-    img_resize = cv2.resize(RGB, (224,224), interpolation = cv2.INTER_NEAREST)
-    #plt.imshow(img_resize)
-    #plt.show()
-    img_p = np.asarray(img_resize, dtype='float32')
-    input_img = np.expand_dims(img_p, axis=0)
+image = load_image('./images/som2.jpg', resize = (224, 224))
+image = image_to_array(image)
+image = preprocessing_img(image)
 
-    #input_img = preprocess_input(input_img)
-
-    input_img = input_img[..., ::-1]
-
-    #input_img /= 127.5
-    #input_img -= 1.
-
-    mean = [103.939, 116.779, 123.68]
-    #std = None
-    input_img[..., 0] -= mean[0]
-    input_img[..., 1] -= mean[1]
-    input_img[..., 2] -= mean[2]
-
-    
-    print('---'*50)
-    print('img_p : ',img_p.shape)
-    print('input_img : ',input_img.shape)
-    return input_img
-
-#model.summary()
-#model = vgg16.vgg16()
-
-
-
-#image = load_img('./images/som2.jpg', target_size=(224, 224))
-#print('PIL image size',image.size)
-#plt.imshow(image)
-#plt.show()
-#numpy_image = img_to_array(image)
-#plt.imshow(np.uint8(numpy_image))
-#plt.show()
-#print('numpy array size',numpy_image.shape)
-#image_batch = np.expand_dims(numpy_image, axis=0)
-#print('image batch size', image_batch.shape)
-#plt.imshow(np.uint8(image_batch[0]))
-#processed_image = preprocess_input(image_batch.copy())
-#processed_image = preprocess_input(numpy_image.copy())
-
-processed_image = preprocessing_img()
 
 model = vgg16_2.vgg16(input_shape=(224, 224, 3))
 model2 = VGG16()
 
-predictions = model.predict(processed_image)
-predictions2 = model2.predict(processed_image)
+predictions = model.predict(image)
+predictions2 = model2.predict(image)
 
 label = decode_predictions(predictions)
 label2 = decode_predictions(predictions2)
--- utils.py
+++ utils.py
@@ -1,14 +1,48 @@
+from __future__ import absolute_import, division, print_function
 import cv2
 import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 
-img = cv2.imread('./images/cat.jpg')
-print(img.shape)
-image_expanded = np.expand_dims(img, axis=0)
-print(image_expanded.shape)
-#cv2.imshow('img',img)
-#print(img)
-#k = cv2.waitKey(0)
-#if k == ord('q'):
-#    cv2.destroyAllWindows()
+def load_image(path, resize = None, resample = 'nearest'):
+    assert type(path) == str, "path to images must be string"
+
+    BGR = cv2.imread(path)
+    out_img = cv2.cvtColor(BGR, cv2.COLOR_BGR2RGB)
+    if resize is not None:
+        assert type(resize) == tuple, "resize must be tuple ex:(width, high)"
+
+        if resample == 'nearest':
+            _interpolation = cv2.INTER_NEAREST
+        elif resample == 'bilinear':
+            _interpolation = cv2.INTER_LINEAR
+        elif resample == 'bicubic':
+            _interpolation = cv2.INTER_CUBIC
+        elif resample == 'lanczos':
+            _interpolation = cv2.INTER_LANCZOS4
+        else:
+            _interpolation = cv2.INTER_AREA
+        
+        out_img = cv2.resize(out_img, (resize[0],resize[1]), interpolation = _interpolation)
+        
+    return out_img
+
+def image_to_array(img):
+    out_arr = np.asarray(img, dtype='float32')
+    out_arr = np.expand_dims(out_arr, axis=0)
+    
+    return out_arr
+
+def preprocessing_img(in_arr):
+    out_arr = in_arr[..., ::-1]
+
+    #input_img /= 127.5
+    #input_img -= 1.
+
+    mean = [103.939, 116.779, 123.68]
+    #std = None
+    out_arr[..., 0] -= mean[0]
+    out_arr[..., 1] -= mean[1]
+    out_arr[..., 2] -= mean[2]
+
+    return out_arr
