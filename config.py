540e1c71f2eed4f705c1d715533b47827c2248c0
wjsunny <jakkapan.sun@hotmail.com>
2020-07-18 00:54:14 +0700

    add config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+IMAGE_ORDERING = "channels_last"
+
+PALETTE = {(0, 0, 0) : 0, (128, 0, 0) : 1, (0, 128, 0) : 2,
+            (128, 128, 0) : 3, (0, 0, 128) : 4, (128, 0, 128) : 5}
+
+NAME_CLASSES = {0 : 'background', 1 : 'cassava', 2 : 'weed', 3 : 'pipe',
+                4 : 'soid', 5 : 'other'}
