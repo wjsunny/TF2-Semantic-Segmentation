ad26469d9ccd382539e98b828cab5a6f6df2174c
wjsunny <jakkapan.sun@hotmail.com>
2020-07-31 17:05:33 +0700

    edit image_format

--- model/backbone/vgg16.py
+++ model/backbone/vgg16.py
@@ -6,38 +6,38 @@ URL_WEIGHTS = 'https://github.com/fchollet/deep-learning-models/releases/downloa
 URL_WEIGHTS_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
 
-def vgg16(classes = 1000, input_shape=(224, 224, 3), include_top = True, pretrained = 'imagenet', IMAGE_FORMAT = 'channels_last'):
+def vgg16(classes = 1000, input_shape=(224, 224, 3), include_top = True, pretrained = 'imagenet', image_format = 'channels_last'):
     inputs = tf.keras.Input(shape = input_shape)
     #Block 1
-    x = layers.Conv2D(64, (3,3), activation = 'relu', padding = 'same', name = 'block1_conv1', data_format = IMAGE_FORMAT)(inputs)
-    x = layers.Conv2D(64, (3,3), activation = 'relu', padding = 'same', name = 'block1_conv2', data_format = IMAGE_FORMAT)(x)
-    x = layers.MaxPooling2D((2,2), strides = 2, name = 'block1_pool', data_format = IMAGE_FORMAT)(x)
+    x = layers.Conv2D(64, (3,3), activation = 'relu', padding = 'same', name = 'block1_conv1', data_format = image_format)(inputs)
+    x = layers.Conv2D(64, (3,3), activation = 'relu', padding = 'same', name = 'block1_conv2', data_format = image_format)(x)
+    x = layers.MaxPooling2D((2,2), strides = 2, name = 'block1_pool', data_format = image_format)(x)
 
     #Block 2
-    x = layers.Conv2D(128, (3,3), activation = 'relu', padding = 'same', name = 'block2_conv1', data_format = IMAGE_FORMAT)(x)
-    x = layers.Conv2D(128, (3,3), activation = 'relu', padding = 'same', name = 'block2_conv2', data_format = IMAGE_FORMAT)(x)
-    x = layers.MaxPooling2D((2,2), strides = 2, name = 'block2_pool', data_format = IMAGE_FORMAT)(x)
+    x = layers.Conv2D(128, (3,3), activation = 'relu', padding = 'same', name = 'block2_conv1', data_format = image_format)(x)
+    x = layers.Conv2D(128, (3,3), activation = 'relu', padding = 'same', name = 'block2_conv2', data_format = image_format)(x)
+    x = layers.MaxPooling2D((2,2), strides = 2, name = 'block2_pool', data_format = image_format)(x)
 
     #Block 3
-    x = layers.Conv2D(256, (3,3), activation = 'relu', padding = 'same', name = 'block3_conv1', data_format = IMAGE_FORMAT)(x)
-    x = layers.Conv2D(256, (3,3), activation = 'relu', padding = 'same', name = 'block3_conv2', data_format = IMAGE_FORMAT)(x)
-    x = layers.Conv2D(256, (3,3), activation = 'relu', padding = 'same', name = 'block3_conv3', data_format = IMAGE_FORMAT)(x)
-    x = layers.MaxPooling2D((2,2), strides = 2, name = 'block3_pool', data_format = IMAGE_FORMAT)(x)
+    x = layers.Conv2D(256, (3,3), activation = 'relu', padding = 'same', name = 'block3_conv1', data_format = image_format)(x)
+    x = layers.Conv2D(256, (3,3), activation = 'relu', padding = 'same', name = 'block3_conv2', data_format = image_format)(x)
+    x = layers.Conv2D(256, (3,3), activation = 'relu', padding = 'same', name = 'block3_conv3', data_format = image_format)(x)
+    x = layers.MaxPooling2D((2,2), strides = 2, name = 'block3_pool', data_format = image_format)(x)
     
     #Block 4
-    x = layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'block4_conv1', data_format = IMAGE_FORMAT)(x)
-    x = layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'block4_conv2', data_format = IMAGE_FORMAT)(x)
-    x = layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'block4_conv3', data_format = IMAGE_FORMAT)(x)
-    x = layers.MaxPooling2D((2,2), strides = 2, name = 'block4_pool', data_format = IMAGE_FORMAT)(x)
+    x = layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'block4_conv1', data_format = image_format)(x)
+    x = layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'block4_conv2', data_format = image_format)(x)
+    x = layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'block4_conv3', data_format = image_format)(x)
+    x = layers.MaxPooling2D((2,2), strides = 2, name = 'block4_pool', data_format = image_format)(x)
     
     #Block 5
-    x = layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'block5_conv1', data_format = IMAGE_FORMAT)(x)
-    x = layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'block5_conv2', data_format = IMAGE_FORMAT)(x)
-    x = layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'block5_conv3', data_format = IMAGE_FORMAT)(x)
-    x = layers.MaxPooling2D((2,2), strides = 2, name = 'block5_pool', data_format = IMAGE_FORMAT)(x)
+    x = layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'block5_conv1', data_format = image_format)(x)
+    x = layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'block5_conv2', data_format = image_format)(x)
+    x = layers.Conv2D(512, (3,3), activation = 'relu', padding = 'same', name = 'block5_conv3', data_format = image_format)(x)
+    x = layers.MaxPooling2D((2,2), strides = 2, name = 'block5_pool', data_format = image_format)(x)
 
     if include_top:
-        x = layers.Flatten(data_format = IMAGE_FORMAT, name = 'flatten')(x)
+        x = layers.Flatten(data_format = image_format, name = 'flatten')(x)
         x = layers.Dense(4096, activation = 'relu', name = 'fc1')(x)
         x = layers.Dense(4096, activation = 'relu', name = 'fc2')(x)
         x = layers.Dense(classes , activation = 'softmax', name = 'predictions')(x)
