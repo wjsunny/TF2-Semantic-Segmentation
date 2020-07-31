d4f619079d2021a417366fbcb85589b23fee5189
wjsunny <jakkapan.sun@hotmail.com>
2020-07-31 17:08:24 +0700

    edit import

--- data_utils.py
+++ data_utils.py
@@ -1,10 +1,6 @@
 from tensorflow.keras.utils import Sequence
 import numpy as np
-import os, sys, inspect
-# currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-# parentdir = os.path.dirname(currentdir)
-# sys.path.insert(0,parentdir) 
-import config
+import os
 from utils import load_mask, load_image
 
 class Dataset:
