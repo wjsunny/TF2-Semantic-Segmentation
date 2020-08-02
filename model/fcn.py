d7807f5b0b1cf00a292ca4e4ed909d1003b61013
wjsunny <jakkapan.sun@hotmail.com>
2020-08-02 16:53:01 +0700

    edit n_classes

--- model/fcn.py
+++ model/fcn.py
@@ -4,8 +4,8 @@ from tensorflow.keras import layers
 import tensorflow as tf
 
 
-def fcn_32(classes, backbone_name='vgg16', input_shape=(224,224,3), image_format = 'channels_last'):
-    backbone = Backbones.get_encoder(name = backbone_name)(classes=classes, input_shape=input_shape, include_top=False)
+def fcn_32(n_classes, backbone='vgg16', input_shape=(224,224,3), image_format = 'channels_last'):
+    backbone = Backbones.get_encoder(name = backbone)(n_classes=n_classes, input_shape=input_shape, include_top=False)
     input = backbone.input
     x = backbone.output
     
@@ -15,9 +15,9 @@ def fcn_32(classes, backbone_name='vgg16', input_shape=(224,224,3), image_format
     x = layers.Conv2D(4096, (1, 1), padding='same', data_format=image_format, 
                         activation='relu')(x)
     x = layers.Dropout(0.5)(x)
-    x = layers.Conv2D(classes, (1, 1), data_format=image_format)(x)
+    x = layers.Conv2D(n_classes, (1, 1), data_format=image_format)(x)
     x = layers.Dropout(0.5)(x)
-    x = layers.Conv2DTranspose(classes, (64, 64), (32, 32), 
+    x = layers.Conv2DTranspose(n_classes, (64, 64), (32, 32), 
         use_bias=False, data_format=image_format)(x)
     x = layers.Activation('softmax')(x)
 
